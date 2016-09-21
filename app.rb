5960f13848389abcb94837b41cbd5f8af1ef395a
Vovanches <vovanches@i.ua>
2016-09-21 17:02:07 +0300

    Init DB and posts table

--- app.rb
+++ app.rb
@@ -1,16 +1,26 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+@db = SQLite3::Database.new 'blog.db'
 
 configure do
+  @db.execute("CREATE TABLE IF NOT EXISTS posts
+            (
+               'id' INTEGER PRIMARY KEY AUTOINCREMENT, 
+               'user_id' INTEGER, 
+               'create_date' DATE, 
+               'content' TEXT
+            )")
 end
 
 before '/visit' do
+
 end
 
 get '/' do
-  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"     
+  erb "Ссылка на базу: #{@db}"#{}"Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"     
 end
 
 get '/about' do
